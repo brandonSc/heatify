afd03af498be0206decc9117b8ee6e7ea5f15a49
Brandon Schurman <schurman@ca.ibm.com>
2016-02-02 12:06:26 -0500

    clean up unused code in app.go

--- app.go
+++ app.go
@@ -1,17 +1,12 @@
 package main
 
 import (
-	//	"fmt"
+	"hub.jazz.net/git/schurman93/Git-Monitor/cadb"
+	"hub.jazz.net/git/schurman93/Git-Monitor/gitutil"
+	"hub.jazz.net/git/schurman93/Git-Monitor/route"
 	"log"
 	"net/http"
 	"os"
-	//"time"
-	//for extracting service credentials from VCAP_SERVICES
-	//"github.com/cloudfoundry-community/go-cfenv"
-	"hub.jazz.net/git/schurman93/Git-Monitor/cadb"
-	//"hub.jazz.net/git/schurman93/Git-Monitor/gitutil"
-	//"hub.jazz.net/git/schurman93/Git-Monitor/model"
-	"hub.jazz.net/git/schurman93/Git-Monitor/route"
 )
 
 const (
@@ -33,23 +28,12 @@ func main() {
 
 	// setup the database
 	cadb.Init()
-	/*
-		// test the database
-		rc := model.RepoCommits{
-			"hub.jazz.net/test/example",
-			time.Now(),
-			"0x1ab234c56d",
-			3,
-		}
-		rc.DbCreate()
-		model.DbRetrieveAllRepoCommits("whatev")
-	*/
 
 	// grab the router and request handlers
 	router := route.NewRouter()
 
-	// run the git update loop in the background
-	//go gitutil.RunUpdateLoop()
+	// run the git repository update loop in the background
+	go gitutil.RunUpdateLoop()
 
 	// launch the server
 	log.Printf("Starting app on %+v:%+v\n", host, port)
